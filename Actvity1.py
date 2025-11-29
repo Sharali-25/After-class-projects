5a4a1afdbd9490572116a134ae62ab2be171afe9
Sharali-25 <Sharaliarkansas@gmail.com>
2025-11-29 11:14:05 -0600

    Add files via upload

---
+++ Actvity1.py
@@ -0,0 +1,16 @@
+var1=a
+var2=c
+var3=v
+
+if var1=a:
+  print("true")
+else:
+  print("incorrect")
+if var2=c:
+  print("true")
+else:
+print("incorrect")
+if var3=v:
+  print("true")
+else:
+print("incorrect")
