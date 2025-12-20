e5aee38c85d3f9a52c81768d726e749a116294c2
Sharali-25 <Sharaliarkansas@gmail.com>
2025-12-19 18:13:49 -0600

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,4 @@
+decimalnumber=(int(input("enter a decimal number: ")))
+binarynumber=bin(decimalnumber)
+binary_number=binarynumber[56]
+print(f"The binary representation of {decimalnumber} is {binary_number}")
