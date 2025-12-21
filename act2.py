0e2cd2d28ca9bca7f0a33ba718e589de586cbc19
Sharali-25 <Sharaliarkansas@gmail.com>
2025-12-21 08:47:14 -0600

    Add files via upload

---
+++ act2.py
@@ -0,0 +1,11 @@
+congrats_1 = "Congratulations on your outstanding exam results! "
+congrats_2 = "Congratulations on your promotion! Wishing you continued success. "
+congrats_3 = "Congratulations on winning the competition! "
+congrats_4 = "Congratulations on your birthday! Have a wonderful year ahead. "
+congrats_5 = "congratulation on playing roblox!"
+print("Congratulations message:\n")
+print(congrats_1)
+print(congrats_2)
+print(congrats_3)
+print(congrats_4)
+print(congrats_5)
