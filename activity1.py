1a28406624a0d1c86d78f2b0e2924759f82fce51
Sharali-25 <Sharaliarkansas@gmail.com>
2025-12-21 08:41:12 -0600

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,7 +1,6 @@
-rows=5
-for i in range(1,rows+1):
-    for j in range(rows-i):
-        print(" " ,end=" ")
-        for k in range(i):
-            print("*",end=" ")
-            print()
+print("half pyramid pattern of stars(*): ")
+n=int(input("enter the number of rows: "))
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
