9a76c4536b2e76a53b27b63f0850f34e2e16c9f7
Sharali-25 <Sharaliarkansas@gmail.com>
2026-02-01 09:41:15 -0600

    Add files via upload

---
+++ acp project.py
@@ -0,0 +1,8 @@
+set6 ={"86"}
+set7 ={"46"}
+print("The difference is 40 ")
+print(set6)
+print(set7)
+print("\n symmertic_difference the the two sets are:")
+set8 =set.symmetric_difference(set6)
+print(set8)
